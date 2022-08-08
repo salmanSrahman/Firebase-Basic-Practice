21b60c71898fadceb20210db07f7cf0dcdd82886
Salman-S-Rahman <salmanmern@gmail.com>
2022-08-08 22:03:00 +0600

    google sign in applied

--- simple-firebase-practice1/src/App.js
+++ simple-firebase-practice1/src/App.js
@@ -1,9 +1,26 @@
+import "./App.css";
+import app from "./firebase.init";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
+const auth = getAuth(app);
 
 function App() {
+  const provider = new GoogleAuthProvider();
+  const handleGoggleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+
   return (
-    <div>
-       <h1>Firebase Authentication</h1>
+    <div className="App">
+      <h1>Auth</h1>
+      <button onClick={handleGoggleSignIn}>Google Sign In</button>
     </div>
   );
 }
---
+++ simple-firebase-practice1/src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB84lVq_q74YC_U17LevH8_db_lO2guf1w",
+  authDomain: "simple-firebase-practice1.firebaseapp.com",
+  projectId: "simple-firebase-practice1",
+  storageBucket: "simple-firebase-practice1.appspot.com",
+  messagingSenderId: "845022364608",
+  appId: "1:845022364608:web:ab85067b8e5238e90bfe25",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
