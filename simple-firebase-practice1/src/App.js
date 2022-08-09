dbbd2430d08e562fe6fb50e35c05b6fc92dd5b38
Salman-S-Rahman <salmanmern@gmail.com>
2022-08-09 11:40:37 +0600

    google sign in

--- simple-firebase-practice1/src/App.js
+++ simple-firebase-practice1/src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
 import app from "./firebase.init";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
+
   const provider = new GoogleAuthProvider();
   const handleGoggleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
         console.log(user);
+        setUser(user);
       })
       .catch((error) => {
         console.log("error", error);
@@ -21,6 +25,9 @@ function App() {
     <div className="App">
       <h1>Auth</h1>
       <button onClick={handleGoggleSignIn}>Google Sign In</button>
+      <h2>{user.displayName}</h2>
+      <h4>{user.email}</h4>
+      <img src={user?.photoURL} alt="" />
     </div>
   );
 }
