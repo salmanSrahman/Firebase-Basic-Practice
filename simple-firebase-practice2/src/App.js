7df988d645b8edbd451390e32eb9249daa9f6c59
Salman-S-Rahman <salmanmern@gmail.com>
2022-08-15 11:39:52 +0600

    all routes set up done

--- simple-firebase-practice2/src/App.js
+++ simple-firebase-practice2/src/App.js
@@ -1,9 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import Order from "./Components/Order/Order";
+import Products from "./Components/Products/Products";
+import Register from "./Components/Register/Register";
 
 function App() {
   return (
     <div>
-      <h1>Firebase Practice</h1>
+      <Header />
+      <Routes path="/" element={<Home />}></Routes>
+      <Routes path="home" element={<Home />}></Routes>
+      <Routes path="products" element={<Products />}></Routes>
+      <Routes path="order" element={<Order />}></Routes>
+      <Routes path="register" element={<Register />}></Routes>
+      <Routes path="login" element={<Login />}></Routes>
     </div>
   );
 }
---
+++ simple-firebase-practice2/src/Components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Container, Navbar, Form, Nav, Button } from "react-bootstrap";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+
+              <Nav.Link href="#" disabled>
+                Link
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
