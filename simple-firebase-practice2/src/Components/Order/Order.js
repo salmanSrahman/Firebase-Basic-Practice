341d99a957bb75e57ee5cee4726aff80329a76be
Salman-S-Rahman <salmanmern@gmail.com>
2022-08-15 11:23:23 +0600

    all components created

---
+++ simple-firebase-practice2/src/Components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Order.css";
+
+const Order = () => {
+  return (
+    <div>
+      <h1>Here ois order details.</h1>
+    </div>
+  );
+};
+
+export default Order;
