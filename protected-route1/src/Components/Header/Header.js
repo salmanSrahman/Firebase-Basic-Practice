fa79f1e76a0cb82e93348cd2f5eb6bc981c39eb6
Salman-S-Rahman <salmanmern@gmail.com>
2022-08-18 23:58:47 +0600

    header part end

--- protected-route1/src/Components/Header/Header.js
+++ protected-route1/src/Components/Header/Header.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import './Header'
+import React from "react";
+import { Navbar, Container, Nav, Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-      return (
-            <div>
-                <h1>Header</h1>  
-            </div>
-      );
+  return (
+    <div>
+      <Navbar bg="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#" className="text-white">
+            Navbar scroll
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="m-auto my-2 my-lg-0 nav__container"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Link to="/home">Home</Link>
+              <Link to="/products">Products</Link>
+              <Link to="/order">Order</Link>
+              <Link to="/register">Register</Link>
+              <Link to="/login">Login</Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
