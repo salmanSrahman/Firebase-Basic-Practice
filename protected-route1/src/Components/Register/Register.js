856305a0c8f8865e8f8cfe726b9da89505abced9
Salman-S-Rahman <salmanmern@gmail.com>
2022-08-20 13:25:50 +0600

    register with email & password

--- protected-route1/src/Components/Register/Register.js
+++ protected-route1/src/Components/Register/Register.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import "./Register.css";
 import { FcGoogle } from "react-icons/fc";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import {
+  useSignInWithGoogle,
+  useCreateUserWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
+
 import auth from "../../firebase.init";
 
 const Register = () => {
   const [signInWithGoogle] = useSignInWithGoogle(auth);
+  const [createUserWithEmailAndPassword] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const navigate = useNavigate();
 
   const handleGoogleSignIn = () => {
@@ -15,24 +26,57 @@ const Register = () => {
       navigate("/home");
     });
   };
+  // handle google sign up
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleConfirmPassword = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleRegisterForm = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Password does not match");
+      return;
+    }
+    createUserWithEmailAndPassword(email, password).then((user) => {
+      navigate("/home");
+    });
+  };
+  // handle create user with email & password
 
   return (
     <div>
       <Container>
         <div className="form__part">
           <h1 className="form__title text-center">Sign Up</h1>
-          <form action="">
+          <form action="" onSubmit={handleRegisterForm}>
             <div className="form__group mb-2">
               <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="" />
+              <input type="email" name="email" id="" onBlur={handleEmail} />
             </div>
             <div className="form__group mb-2">
               <label htmlFor="email">Password</label>
-              <input type="password" name="password" id="" />
+              <input
+                type="password"
+                name="password"
+                id=""
+                onBlur={handlePassword}
+              />
             </div>
             <div className="form__group mb-2">
               <label htmlFor="email">Confirm Password</label>
-              <input type="password" name="password" id="" />
+              <input
+                type="password"
+                name="password"
+                id=""
+                onBlur={handleConfirmPassword}
+              />
             </div>
             <button className="signup__btn">Sign Up</button>
             <div className="text-center transfer__login pt-1">
